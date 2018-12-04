7b3e7c54f3d2ba3996014ddbdf173cc8e2b54ae4
kriswang <zhaosen2@illinois.edu>
2018-12-03 20:19:38 -0600

    hey

--- app/src/main/java/com/example/kriswang/mp6/MainActivity.java
+++ app/src/main/java/com/example/kriswang/mp6/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.kriswang.mp6;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageButton b001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b001 = findViewById(R.id.b001);
+        b001.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityp001();
+            }
+        });
+    }
+
+    public void openActivityp001() {
+        Intent intent = new Intent(this, p001.class);
+        startActivity(intent);
     }
 }
