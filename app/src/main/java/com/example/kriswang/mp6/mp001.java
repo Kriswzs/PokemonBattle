27b96a0cb0981a704681fbd9382199eb258149c6
kriswang <zhaosen2@illinois.edu>
2018-12-11 01:53:09 -0600

    hey

--- app/src/main/java/com/example/kriswang/mp6/mp001.java
+++ app/src/main/java/com/example/kriswang/mp6/mp001.java
@@ -2,6 +2,8 @@ package com.example.kriswang.mp6;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class mp001 extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class mp001 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mp001);
+
+        WebView wv = (WebView) findViewById(R.id.wv);
+        wv.loadUrl("https://www.youtube.com/watch?v=p9jY5clw3P0");
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.setWebViewClient(new WebViewClient());
+
     }
 }
